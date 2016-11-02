69e4e1e5e8c89f57425a574898374ef69b7986df
mwhite30 <mjdev3030@gmail.com>
2016-11-02 14:45:47 -0400

    This is the 3rd commit

---
+++ app/src/main/java/edu/temple/m/colorpicker/CanvasFragment.java
@@ -0,0 +1,21 @@
+package edu.temple.m.colorpicker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by M on 11/1/2016.
+ */
+
+public class CanvasFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        //Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_palette, container, false);
+    }
+}
